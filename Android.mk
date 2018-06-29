58559dfcc8b93a2ab7068c9da9ed71b439a19424
Akhil's Lazy Buildbot <jenkins@akhilnarang.me>
2018-06-29 14:27:01 -0700

    add

---
+++ Android.mk
@@ -0,0 +1,14 @@
+#LOCAL_PATH:= $(call my-dir)
+#
+#include $(CLEAR_VARS)
+#
+#LOCAL_JAVA_LIBRARIES := org.dirtyunicorns.utils
+#
+#LOCAL_SRC_FILES := $(call all-java-files-under, src)
+#
+#LOCAL_MODULE_TAGS := optional
+#
+#LOCAL_MODULE := org.dirtyunicorns.navigation-static-internal
+#
+#include $(BUILD_STATIC_JAVA_LIBRARY)
+
